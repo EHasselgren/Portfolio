f7a944de6bd5057ead46ac0af1b2859599f72d74
elias hasselgren <elias.hasselgren@icloud.com>
2024-10-29 17:37:50 +0100

    design update

--- my-portfolio/src/components/buttons/ScrollButton.tsx
+++ my-portfolio/src/components/buttons/ScrollButton.tsx
@@ -8,12 +8,12 @@ interface ScrollButtonProps {
 
 export const ScrollButton: React.FC<ScrollButtonProps> = ({ onScrollClick }) => {
   return (
-    <button
-      onClick={onScrollClick}
-      className="animate-bounce p-3 rounded-full mt-8 bg-gradient-to-br from-purple-400 to-cyan-600 text-white shadow-md hover:shadow-lg transition-shadow duration-300 w-12 h-12 flex items-center justify-center"
-      aria-label="Scroll down"
-    >
-      <FontAwesomeIcon icon={faChevronDown} className="w-5 h-5" />
-    </button>
+<button
+  onClick={onScrollClick}
+  className="animate-bounce w-12 h-12 rounded-full mt-8 bg-gradient-to-br from-purple-400 to-cyan-600 text-white shadow-md hover:shadow-lg transition-shadow duration-300 flex items-center justify-center"
+  aria-label="Scroll down"
+>
+  <FontAwesomeIcon icon={faChevronDown} className="w-6 h-6" />
+</button>
   );
 };
