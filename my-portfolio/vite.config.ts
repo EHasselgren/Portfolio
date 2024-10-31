8065b592f2aa51657898b982a158c763ca9d5620
elias hasselgren <elias.hasselgren@icloud.com>
2024-10-31 17:13:28 +0100

    Vite setup

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // Replace 'Portfolio' with your repository name
+  base: '/Portfolio/',
 })
