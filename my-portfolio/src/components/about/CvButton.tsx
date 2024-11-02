29c3181306f0d45c1b2a323c576badca58f3a34c
Elias Hasselgren <Elias.hasselgren@icloud.com>
2024-11-02 09:00:35 +0100

    Updated cv url

--- my-portfolio/src/components/about/CvButton.tsx
+++ my-portfolio/src/components/about/CvButton.tsx
@@ -5,7 +5,7 @@ import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
 export const CvButton: React.FC = () => {
   return (
     <a
-      href="https://ehasselgren.github.io/CV_project/"
+      href="https://ehasselgren.github.io/CV/"
       target="_blank"
       rel="noopener noreferrer"
       className="animate-pulse px-4 py-3 rounded-full bg-gradient-to-br from-purple-400 to-cyan-600 text-white shadow-md hover:shadow-lg hover:from-purple-500 hover:to-cyan-700 transition-all duration-300 flex items-center gap-2"
